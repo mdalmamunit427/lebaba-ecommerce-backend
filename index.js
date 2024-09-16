b589d91b1f07dffc5a73dbe03ffd684572fb9064
Md Al Mamun <96342744+mdalmamunit427@users.noreply.github.com>
2024-09-17 02:18:58 +0600

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));  
 
 app.use(cors({ 
-  origin: 'https://lebaba-ecommerce-frontend.vercel.app',
+  origin: 'https://www.protonier.xyz',
   credentials: true,
 }));
 
